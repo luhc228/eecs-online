721e8c79fbc79b65bf761d49963fa682d7058fa9
luhc228 <luhengchang228@gmail.com>
2019-11-07 20:12:34 +0800

    feat: add redirect

--- eecs-online-client/src/pages/login/models/index.ts
+++ eecs-online-client/src/pages/login/models/index.ts
@@ -1,5 +1,7 @@
+import { routerRedux } from 'dva/router';
 import * as service from '../services';
 import { USER_TYPE } from '@/enums';
+import { getPageQuery } from '@/utils/utils';
 
 export default {
   namespace: 'login',
@@ -18,7 +20,7 @@ export default {
   },
 
   effects: {
-    *userLogin({ payload: { userType, value } }: any, { call }: any) {
+    *userLogin({ payload: { userType, value } }: any, { call, put }: any) {
       let userLogin;
       switch (userType) {
         case USER_TYPE.Student:
@@ -32,7 +34,40 @@ export default {
       }
 
       const response = yield call(userLogin, value);
-      console.log(response);
+
+      const urlParams = new URL(window.location.href);
+      const params = getPageQuery();
+      let { redirect } = params as { redirect: string };
+      if (redirect) {
+        const redirectUrlParams = new URL(redirect);
+        if (redirectUrlParams.origin === urlParams.origin) {
+          redirect = redirect.substr(urlParams.origin.length);
+          if (redirect.match(/^\/.*#/)) {
+            redirect = redirect.substr(redirect.indexOf('#') + 1);
+          }
+        } else {
+          window.location.href = redirect;
+          return;
+        }
+      }
+
+      const pathnamePrefix = userType;
+      yield put(routerRedux.replace(redirect || `${pathnamePrefix}`));
+    },
+
+    *logout(_, { put }) {
+      // const { redirect } = getPageQuery();
+      // // redirect
+      // if (window.location.pathname !== '/user/login' && !redirect) {
+      //   yield put(
+      //     routerRedux.replace({
+      //       pathname: '/user/login',
+      //       search: stringify({
+      //         redirect: window.location.href,
+      //       }),
+      //     }),
+      //   );
+      // }
     },
   },
 
