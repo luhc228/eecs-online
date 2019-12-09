181905afc52929255e808f5858f9e67cda4c83b9
Hengchang Lu <luhengchang228@gmail.com>
2019-12-09 16:34:47 +0800

    feat: add Prompt and redux-persist

--- eecs-online-client/src/components/RouterPrompt/index.tsx
+++ eecs-online-client/src/components/RouterPrompt/index.tsx
@@ -6,6 +6,7 @@ export default () => (
     <span>提示</span>
     <Prompt
       when
+      // message={location => window.confirm(`confirm to leave to ${location.pathname}?`)}
       message={() => window.confirm('当前页面数据尚未保存，是否确定离开？')}
     />
   </>
