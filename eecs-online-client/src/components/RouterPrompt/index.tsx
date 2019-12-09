5c6c356378f8bf1e1503051223fb2519b4091c16
Hengchang Lu <luhengchang228@gmail.com>
2019-12-09 17:12:37 +0800

    chore: remove cookie

--- eecs-online-client/src/components/RouterPrompt/index.tsx
+++ eecs-online-client/src/components/RouterPrompt/index.tsx
@@ -4,7 +4,7 @@ import Prompt from 'umi/prompt';
 export default () => (
   <>
     <Prompt
-      when
+      when={true}
       // message={location => window.confirm(`confirm to leave to ${location.pathname}?`)}
       message={() => window.confirm('当前页面数据尚未保存，是否确定离开？')}
     />
