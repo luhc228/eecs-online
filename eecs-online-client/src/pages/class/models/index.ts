39744d89a431c996ea2ecb3b3cfa202b0f2a06a6
Jeanil-Gao <201700121036@mail.sdu.edu.cn>
2019-12-10 20:49:38 +0800

    change class

--- eecs-online-client/src/pages/class/models/index.ts
+++ eecs-online-client/src/pages/class/models/index.ts
@@ -3,4 +3,10 @@ import { ClassTableData, ClassFieldsModal } from '@/interfaces/class';
 export interface StateType {
     data: ClassTableData;
     filterFields: ClassFieldsModal;
-  }
+}
+
+export type Effect = (
+  action: AnyAction,
+  effects: EffectsCommandMap & { select: <T>(func: (state: StateType) => T) => T },
+) => void;
+
