9afbf7537d4c70fe5987e5baa81c1deeb84a36ae
Hengchang Lu <luhengchang228@gmail.com>
2019-12-19 19:50:29 +0800

    feat: update questionLibEdit

---
+++ eecs-online-client/src/pages/questionLibEdit/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'dva';
+
+const QuestionLibEdit: React.FC<{}> = () => (
+    <div></div>
+  )
+
+// const mapStateToProps = ({
+//   questionLib
+// }: {
+//   questionLib: StateType
+// }) => ({
+//   questionLib
+// });
+
+export default connect()(QuestionLibEdit);
