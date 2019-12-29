1f34129462a0e37f6956766749cf160ace859f50
luhc228 <luhengchang228@gmail.com>
2019-12-29 08:37:33 +0800

    feat: update hader

--- eecs-online-client/src/pages/studentHomeworkEdit/components/Header/index.tsx
+++ eecs-online-client/src/pages/studentHomeworkEdit/components/Header/index.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Button } from 'antd';
 import { StateType } from '../../models';
 import { UmiComponentProps } from '@/interfaces/components';
+import styles from './index.less';
 
 export interface HeaderProps extends UmiComponentProps {
-
+  studentHomeworkEdit: StateType,
 }
 
-const Header: React.FC<{}> = () => {
-  console.log(111);
-
+const Header: React.FC<HeaderProps> = ({
+  dispatch,
+  studentHomeworkEdit
+}) => {
+  const { data } = studentHomeworkEdit;
+  const { homeworkScore } = data;
   return (
-    <div>
+    <div className={styles.header}>
+      <div className={styles.content}>
+        {homeworkScore}
+      </div>
+      <Button
+        type="primary"
+        onClick={() => {
 
+        }}
+      >
+        暂存答案
+      </Button>
     </div>
   )
 }
 
 const mapStateToProps = ({
   studentHomeworkEdit,
-
+  loading
 }: {
   studentHomeworkEdit: StateType,
   loading: any,
 }) => ({
   studentHomeworkEdit,
-  loading: true
+  loading: loading.model.studentHomeworkEdit
 })
 
 export default connect(mapStateToProps)(Header);
